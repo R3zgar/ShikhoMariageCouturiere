e80fd6ba953dc7ed411fce300542647cd7c4731b
R3zgar <rzgarevam05@gmail.com>
2024-08-22 01:23:46 +0200

    Ajout systéme Routage

---
+++ Router/allRoutes.js
@@ -0,0 +1,15 @@
+import Route from "./Route.js";
+
+//Définir ici vos routes
+export const allRoutes = [
+  new Route("/", "Accueil", "/pages/home.html"),
+  /** 
+  new Route("/", "Propose", "/pages/propose.html"),
+  new Route("/", "Services", "/pages/services.html"),
+  new Route("/Galerie", "Galerie", "/pages/galerie.html", "/js/galerie.js"),
+  new Route("/", "Contact", "/pages/contact.html"),
+  **/
+];
+
+//Le titre s'affiche comme ceci : Route.titre - websitename
+export const websiteName = "Shikho Mariage Couturiere";
