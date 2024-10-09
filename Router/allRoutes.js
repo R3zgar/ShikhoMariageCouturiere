c78de2a929807ab5343185094c0c6911cbdbcc24
R3zgar <rzgarevam05@gmail.com>
2024-10-09 23:41:18 +0200

    je fais allroutes pour authorize et modifer all page outhorize,

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,16 +2,16 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-  new Route("/", "Accueil", "/pages/home.html"),
-  new Route("/galerie", "La galerie", "/pages/galerie.html"),
-  new Route("/aproposer", "À Proposer", "/pages/aproposer.html"),
-  new Route("/contact", "Contact", "/pages/contact.html", "/js/contact.js"),
-  new Route("/signin", "Connexion", "/pages/auth/signin.html"),
-  new Route("/signup", "Inscription", "/pages/auth/signup.html", "/js/auth/signup.js"),
-  new Route("/account", "Mon compte", "/pages/auth/account.html"),
-  new Route("/editPassword", "Chengement de mot de passe", "/pages/auth/editPassword.html"),
-  new Route("/allCommandes", "Vos commandes", "/pages/commandes/allCommandes.html"),
-  new Route("/commander", "Commander", "/pages/commandes/commander.html"),
+  new Route("/", "Accueil", "/pages/home.html", []),
+  new Route("/galerie", "La galerie", "/pages/galerie.html", [], "/js/galerie.js"),
+  new Route("/aproposer", "À Proposer", "/pages/aproposer.html", []),
+  new Route("/contact", "Contact", "/pages/contact.html", [], "/js/contact.js"),
+  new Route("/signin", "Connexion", "/pages/auth/signin.html", ["disconnected"], "/js/auth/signin.js"),
+  new Route("/signup", "Inscription", "/pages/auth/signup.html", ["disconnected"], "/js/auth/signup.js"),
+  new Route("/account", "Mon compte", "/pages/auth/account.html", ["client", "admin"]),
+  new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html", ["client", "admin"]),
+  new Route("/allCommandes", "Vos commandes", "/pages/commandes/allCommandes.html", ["client"]),
+  new Route("/commander", "Commander", "/pages/commandes/commander.html", ["client"]),
 
 ];
 
--- Router/router.js
+++ Router/router.js
@@ -26,6 +26,25 @@ const LoadContentPage = async () => {
   const path = window.location.pathname;
   // Récupération de l'URL actuelle
   const actualRoute = getRouteByUrl(path);
+
+  //Vérifier les droits d'accès à la page
+  const allRolesArray = actualRoute.authorize;
+
+  if(allRolesArray.length > 0){
+    if(allRolesArray.includes("disconnected")){
+      if(isConnected()){
+        window.location.replace("/");
+      }
+    }
+    else{
+      const roleUser = getRole();
+      if(!allRolesArray.includes(roleUser)){
+        window.location.replace("/");
+      }
+    }
+  } 
+
+
   // Récupération du contenu HTML de la route
   const html = await fetch(actualRoute.pathHtml).then((data) => data.text());
   // Ajout du contenu HTML à l'élément avec l'ID "main-page"
@@ -42,8 +61,11 @@ const LoadContentPage = async () => {
     document.querySelector("body").appendChild(scriptTag);
   }
 
-  // Changement du titre de la page
-  document.title = actualRoute.title + " - " + websiteName;
+// Changement du titre de la page
+document.title = actualRoute.title + " - " + websiteName;
+
+//Afficher et masquer les éléments en fonction du rôle
+showAndHideElementsForRoles();
 };
 
 // Fonction pour gérer les événements de routage (clic sur les liens)
